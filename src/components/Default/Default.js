eafde7fda49993052d621d0764531451ee7dec48
Lenny Peters <mr.lennypeters@gmail.com>
2018-01-21 17:26:56 -0800

    master - refactor - fixing the select wheel

--- src/components/Default/Default.js
+++ src/components/Default/Default.js
@@ -8,10 +8,10 @@ class Default extends Component {
                 <h2>Posts</h2>
                 <button>Add Post</button>
                 <br/>
+                <label htmlFor="">Filter by: </label>
                 <select name="" id="">
-                    <option value="">Select a Filter</option>
-                    <option value="">Filter by timeStamp</option>
-                    <option value="">Filter by voteScore</option>
+                    <option value="">timeStamp</option>
+                    <option value="">voteScore</option>
                 </select>
                 <hr/>
                 <div className="post-container">
@@ -19,6 +19,9 @@ class Default extends Component {
                         <h3><a href="">Some Post</a></h3>
                         <small>author | timeStamp | voteScore</small>
                         <p>Maecenas sed diam eget risus varius blandit sit amet non 
+                            magna. Vivamus sagittis lacus vel augue laoreet rutrum 
+                            faucibus dolor auctor.
+                            Maecenas sed diam eget risus varius blandit sit amet non 
                             magna. Vivamus sagittis lacus vel augue laoreet rutrum 
                             faucibus dolor auctor.</p>
                     </article>
