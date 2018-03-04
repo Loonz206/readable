c8e551431de777a0d7e88109a6822295f7466d4b
Lenny Peters <mr.lennypeters@gmail.com>
2018-03-03 18:46:10 -0800

    master - mother of god..

--- src/components/DefaultView/DefaultView.js
+++ src/components/DefaultView/DefaultView.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Postlist from '../../components/PostList/Postlist';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../../actions/index';
 
-const DefaultView = (props) => (
-    <div className="default-container">
-        <h1>Welcome to Readable</h1>
-        <h2>Posts</h2>
-        <Link to='/create-post'>
-            <button>Add Post</button>
-        </Link>
-        <br/>
-        <label htmlFor="">Filter by: </label>
-        <select name="" id="">
-            <option value="">timeStamp</option>
-            <option value="">voteScore</option>
-        </select>
-        <hr/>
-        <Postlist />
-    </div>
-)
+class DefaultView extends Component {
 
-export default DefaultView;
+    componentDidMount(){
+        
+    }
+
+    render () {
+        return (
+            <div className="default-container">
+                <h1>Welcome to Readable</h1>
+                <h2>Posts</h2>
+                <Link to='/create-post'>
+                    <button>Add Post</button>
+                </Link>
+                <br/>
+                <label htmlFor="">Filter by: </label>
+                <select name="" id="">
+                    <option value="">timeStamp</option>
+                    <option value="">voteScore</option>
+                </select>
+                <hr/>
+            </div>
+        )
+    }
+} 
+    
+const mapStateToProps = ({ posts }) => ({
+    posts: posts.posts
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    getPosts: () => dispatch(fetchPosts())
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(DefaultView);
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const Post = (props) => (
-    <article>
-        <h3><a href="">title</a></h3>
-        <small>author | timestamp | voteScore</small>
-        <p>category</p>
-        <p>body</p>
-    </article>
+    <div className="post-container">
+        <p>Some Generic Post</p>
+    </div>
 )
 
 export default Post;
